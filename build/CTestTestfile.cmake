41101ba14ad1fd0919773fde9190fab706d954a3
zzparkur <vovasharendo@gmail.com>
2025-09-15 02:18:40 +0300

    fix

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/d/labs/laba2/rpn_calculator
-# Build directory: /mnt/d/labs/laba2/rpn_calculator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
